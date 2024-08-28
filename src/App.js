fda6cd818148beaaa4e5813f7ce7733fd79b61f5
mwinamijr <mwinamijr@gmail.com>
2024-08-28 15:43:37 +0300

    added page functionalities

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import {HashRouter as Router, Routes, Route} from 'react-router-dom'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import Resume from './screens/Resume';
+import Services from './screens/Services';
+//import Testimonials from './screens/Testimonials';
 import AboutScreen from './screens/AboutScreen';
 import ContactScreen from './screens/ContactScreen';
 
@@ -13,7 +16,9 @@ function App() {
       <Routes to="/">
         <Route index element={<HomeScreen />} />
         <Route path='/about' element={<AboutScreen />} />
-        <Route path='/contact-us' element={<ContactScreen />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/services" element={<Services />} />
+        <Route path='/contact' element={<ContactScreen />} />
       </Routes>
       <Footer />
     </Router>
