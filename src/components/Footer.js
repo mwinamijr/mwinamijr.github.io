7c3d5f21005e59107e5ac0764e9a5c5f320ecece
mwinamijr <mwinamijr@gmail.com>
2024-08-28 20:46:59 +0300

    added js navbar collpse

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <footer className="bg-light text-center text-lg-start">
-      <div className="social-links text-center">
+    <footer className="bg-dark text-center text-lg-start">
+      <div className="social-links text-center pt-3">
         <Link to="https://x.com/mwinami_" className="twitter p-1"><i className="bi bi-twitter-x"></i></Link>
         <Link to="https://facebook.com/mwinamijr" className="facebook p-1"><i className="bi bi-facebook"></i></Link>
         <Link to="https://instagram.com/_mwinami" className="instagram p-1"><i className="bi bi-instagram"></i></Link>
         <Link to="https://linkedin.com/in/mwinamijr" className="linkedin p-1"><i className="bi bi-linkedin"></i></Link>
         <Link to="https://github.com/mwinamijr" className="github p-1"><i className="bi bi-github"></i></Link>
       </div>
-      <div className="text-center p-3">
+      <div className="text-center p-3" style={{color: '#fff'}}>
         <p>
           &copy; {new Date().getFullYear()} Techdometz. All rights reserved. <br />Designed by Mwinami Jr.
         </p>
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
         <Link className="navbar-brand" to="/">Athumani Mwinami</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
